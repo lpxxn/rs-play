76bbdd9b54a5e739da09a1cdbe77e5dfd18fc541
lpxxn <mi_duo@live.com>
2023-11-01 10:22:35 +0800

    feat: hello

--- ahello/src/main.rs
+++ ahello/src/main.rs
@@ -13,6 +13,7 @@ fn main() {
     assert_eq!(b, false);
 
     assign_a1();
+    reassign();
 }
 
 struct Student {
@@ -31,4 +32,27 @@ fn assign_a1() {
     Student {e, ..} = Student {e: 1, n: "a".to_string()};
     // 只初始化e，忽略n
     println!("a = {:?}, b = {:?}, c = {:?}, d = {:?}, e = {:?}", a, b, c, d, e);
+}
+
+fn reassign() {
+    let x = 5;
+    let x = x +1;
+    {
+        let x = x * 2;
+        println!("The value of x is :{}", x);
+    }
+
+    println!("The value of x is :{}", x);
+
+    let spaces: &str = "   ";
+    let spaces: usize= spaces.len();
+    println!("The value of spaces is :{}", spaces);
+
+    // let mut spaces = "   ";
+    // spaces = spaces.len(); // expected `&str`, found `usize`
+    /*
+    这和 mut 变量的使用是不同的，第二个 let 生成了完全不同的新变量，
+    两个变量只是恰好拥有同样的名称，涉及一次内存对象的再分配 ，
+    而 mut 声明的变量，可以修改同一个内存地址上的值 
+    */
 }
