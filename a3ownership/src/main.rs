226a4a1632ba48b49876b5498c24bd6b61617ddb
lpxxn <mi_duo@live.com>
2023-11-02 10:51:20 +0800

    feat: owner ship

--- a3ownership/src/main.rs
+++ a3ownership/src/main.rs
@@ -19,3 +19,10 @@ fn makes_copy(some_integer: i32) { // some_integer 进入作用域
     println!("{}", some_integer);
 } // some_integer 移出作用域，不会有特殊操作
 
+// 同样的，函数返回值也有所有权，例如:
+fn gives_ownership() -> String {                       // gives_ownership 将返回值移动给
+                                                       // 调用它的函数
+    let some_string = String::from("hello"); // some_string 进入作用域.
+
+    some_string                                         // 返回 some_string 并移出给调用的函数
+} // some_string 栈上的内存 moved 到调用的函数,some_string 不再指向栈上的内存，即不再有效
