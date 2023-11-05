cdd46c15d8423fe3276a22adf4fe5f77c255e3cf
Aiden <mi_duo@live.com>
2023-11-05 22:28:26 +0800

    feat: ownership

--- baseic/a4string/src/main.rs
+++ baseic/a4string/src/main.rs
@@ -8,4 +8,25 @@ fn main() {
     let us1 = utf8_slice::from(s.as_str(), 2);
     println!("{}", us1);
     println!("len: {}", utf8_slice::len(&s[..]));
+
+    println!("index 1 {}", utf8_slice::slice(&s, 0, 1));
+
+    str_1();
+}
+
+
+fn str_1() {
+    // 正常情况下我们无法使用 str 类型，但是可以使用 &str 来替代
+    let s: &str = "hello world";
+
+    // 如果要使用 str 类型，只能配合 Box。 & 可以用来将 Box<str> 转换为 &str 类型
+    let s: Box<str> = "hello world".into();
+    greetings(&s);
+
+    let s: Box<&str> = Box::new("hello world");
+    greetings(*s);
+}
+
+fn greetings(s: &str) {
+    println!("hello {}", s);
 }
