03967b209332a062af51f8c72a701ae7273802bb
Aiden <mi_duo@live.com>
2023-11-05 21:48:22 +0800

    feat: ownership

---
+++ baseic/a4string/readme.md
@@ -0,0 +1,11 @@
+## utf8
+
+```
+Run the following Cargo command in your project directory:
+
+cargo add utf8_slice
+
+Or add the following line to your Cargo.toml:
+
+utf8_slice = "1.0.0"
+```
--- baseic/a4string/src/main.rs
+++ baseic/a4string/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     let hello = &s[..5];
     let world = &s[6..11];
     println!("{} {}", hello, world);
+
+    let s = String::from("你好世界");
+    let us1 = utf8_slice::from(s.as_str(), 2);
+    println!("{}", us1);
+    println!("len: {}", utf8_slice::len(&s[..]));
 }
