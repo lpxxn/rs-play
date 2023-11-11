fcbf23880890b974cd003f7499dc68d6f28f40ff
Aiden <mi_duo@live.com>
2023-11-11 23:12:34 +0800

    feat: ownership

---
+++ c1basic/a3trait1/src/a1summary.rs
@@ -0,0 +1,63 @@
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+    // 默认实现允许调用相同特征中的其他方法，哪怕这些方法没有默认实现。如此，特征可以提供很多有用的功能而只需要实现指定的一小部分内容
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct Post {
+    pub title: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for Post {
+    fn summarize_author(&self) -> String {
+        format!("author: {}", self.author)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{} by {} ", self.title, self.author)
+    }
+}
+
+pub struct Weibo {
+    pub username: String,
+    pub content: String,
+}
+
+impl Summary for Weibo {
+    fn summarize_author(&self) -> String {
+        format!("username: {}", self.username)
+    }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+#[cfg(test)]
+mod test_summary {
+    use super::*;
+
+    #[test]
+    fn test_summary() {
+        let post = Post {
+            title: "title".to_string(),
+            author: "author".to_string(),
+            content: "content".to_string(),
+        };
+        println!("post: {}", post.summarize());
+
+        let weibo = Weibo {
+            username: "li".to_string(),
+            content: "haha rust".to_string(),
+        };
+        println!("weibo: {}", weibo.summarize());
+
+        println!("notify post: ---------------");
+        notify(&post);
+        notify(&weibo);
+    }
+}
---
+++ c1basic/a3trait1/src/main.rs
@@ -0,0 +1,5 @@
+mod a1summary;
+
+fn main() {
+    println!("Hello, world!");
+}
