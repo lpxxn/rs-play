5e291c4e6565c92619daa208ce56f9de8f683f3f
Aiden <mi_duo@live.com>
2023-11-19 22:30:35 +0800

    feat: option

--- c1basic/a6error/src/main.rs
+++ c1basic/a6error/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 use std::net::IpAddr;
 
 fn main() {
@@ -6,6 +7,7 @@ fn main() {
     // let h: IpAddr = "123".parse().unwrap(); // 因为格式不对，所以会 panic
 
     open_file();
+    open_file2();
 }
 
 
@@ -22,11 +24,19 @@ fn open_file() {
 
 fn open_file2() {
     let f = File::open("hello.txt");
-    let fileObj: File;
+    let mut fileObj: Option<File> = Option::None;
     match f {
-        Ok(file) => fileObj = file,
-        Err(error) => match error. {
-            println!("Problem opening the file: {:?}", error);
+        Ok(file) => fileObj = Option::Some(file),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => {
+                println!("not found");
+                match File::create("hello.txt") {
+                    Ok(fc) => fileObj = Option::Some(fc),
+                    Err(e) => panic!("error {}", e),
+                }
+            }
+            other_err => println!("Problem opening the file: {:?}", other_err)
         }
     };
+    println!("fileObj {:?}", fileObj);
 }
