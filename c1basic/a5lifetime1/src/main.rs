39251669272224ee74c31ab5d0407cf92d2b7f74
lpxxn <mi_duo@live.com>
2023-11-20 10:51:19 +0800

    feat: lifetime

--- c1basic/a5lifetime1/src/main.rs
+++ c1basic/a5lifetime1/src/main.rs
@@ -2,16 +2,29 @@ mod l1;
 mod l2;
 
 fn main() {
-    println!("Hello, world!");
+    let a = "abc";
+    let b = invalid_output3();
+    println!("a: {}", a);
+    println!("b: {}", b);
+    let c = get_str();
+    println!("c: {}", c);
 }
 
 
 fn invalid_output() -> String {
     String::from("foo")
 }
+
 fn invalid_output2() -> &'static str {
     "foo"
 }
+
 fn invalid_output3<'a>() -> &'a str {
     "foo"
+}
+
+// return &str
+fn get_str<'a>() -> &'a str {
+    let s = "abcd";
+    s
 }
