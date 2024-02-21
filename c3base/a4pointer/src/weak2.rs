c84b359437daed4b246c20720347b5aad6cf1548
lpxxn <mi_duo@live.com>
2024-02-21 16:34:33 +0800

    feat: refcell

--- c3base/a4pointer/src/weak2.rs
+++ c3base/a4pointer/src/weak2.rs
@@ -2,18 +2,18 @@ use std::rc::Rc;
 
 #[test]
 fn fn_test_weak1() {
-    let five = Rc::new(5);
+    let five: Rc<i32> = Rc::new(5);
     println!("five = {:?}", five);
     let weak_five = Rc::downgrade(&five);
 
     // 有个疑问，这里如果注释掉，下面 drop(five) 后， weak_five.upgrade() 还能获取到值，为什么？
     // 因为如果还有Rc的引用，那么weak_five.upgrade()就能获取到值
     // 如果还有Rc的引用， drop(five) 也不会释放内存
-
     let strong_five: Option<Rc<_>> = weak_five.upgrade();
     println!("strong_five = {:?}", strong_five);
-    assert_eq!(5, *strong_five.unwrap());
-
+    // assert_eq!(Rc::new(5), strong_five.unwrap());
+    // assert_eq!(5, *strong_five.unwrap());
+    let _ = strong_five.unwrap();
 
     let strong_five: Option<Rc<_>> = weak_five.upgrade();
     println!("strong_five = {:?}", strong_five);
@@ -21,8 +21,9 @@ fn fn_test_weak1() {
     // println!("ref count = {}", Rc::strong_count(strong_five.as_ref().unwrap()));
     match strong_five.as_ref() {
         None => { println!("None can't be counted!") }
-        Some(rc) => { println!("ref count = {}", Rc::strong_count(rc)); }
+        Some(rc) => { println!("2 ref count = {}", Rc::strong_count(rc)); }
     }
+    let _ = strong_five.unwrap();
     drop(five);
 
 
@@ -30,9 +31,11 @@ fn fn_test_weak1() {
     println!("strong_five = {:?}", strong_five);
     match strong_five.as_ref() {
         None => { println!("None can't be counted!") }
-        Some(rc) => { println!("ref count = {}", Rc::strong_count(rc)); }
+        Some(rc) => {
+            println!("3 ref count = {}", Rc::strong_count(rc));
+            assert_eq!(5, *strong_five.unwrap());
+        }
     }
-    assert_eq!(5, *strong_five.unwrap());
 
     let strong_five: Option<Rc<_>> = weak_five.upgrade();
     println!("strong_five = {:?}", strong_five);
