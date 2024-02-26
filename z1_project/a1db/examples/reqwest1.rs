0c825ffdb8d32b6057d0eb0ada1b67910c418f68
lpxxn <mi_duo@live.com>
2024-02-26 17:20:05 +0800

    feat: toml

--- z1_project/a1db/examples/reqwest1.rs
+++ z1_project/a1db/examples/reqwest1.rs
@@ -1,4 +1,32 @@
 /*
 https://github.com/seanmonstar/reqwest
  */
-fn main() {}
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    ip: String,
+    port: Option<u16>,
+    keys: Keys,
+}
+
+#[derive(Deserialize, Debug)]
+struct Keys {
+    github: String,
+    travis: Option<String>,
+}
+
+fn main() {
+    let config: Config = toml::from_str(r#"
+        ip = '127.0.0.1'
+
+        [keys]
+        github = 'xxxxxxxxxxxxxxxxx'
+        travis = 'yyyyyyyyyyyyyyyyy'
+    "#).unwrap();
+    println!("{:?}", config);
+    assert_eq!(config.ip, "127.0.0.1");
+    assert_eq!(config.port, None);
+    assert_eq!(config.keys.github, "xxxxxxxxxxxxxxxxx");
+    assert_eq!(config.keys.travis.as_ref().unwrap(), "yyyyyyyyyyyyyyyyy");
+}
